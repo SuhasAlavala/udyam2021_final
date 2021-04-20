7c26c111a527ac3662b3761909dd08fcb6ac7099
SuhasAlavala <suhasalavala@gmail.com>
2021-04-20 15:16:31 +0530

    user search bar added

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from authentication.models import User, Query
 
-admin.site.register(User)
+class User_Admin(admin.ModelAdmin):
+    model = User
+    search_fields = ['user__phone']
+    list_display = ("username", "College_name", "Year")
+
+admin.site.register(User, User_Admin)
 admin.site.register(Query)
